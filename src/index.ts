98152ebf7dfa48f6e02391db37a8554a1dfae06e
Pankaj <pnkjsngh564@gmail.com>
2025-05-24 00:53:05 +0530

    cors error fix

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ declare global {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://slooze-frontend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
